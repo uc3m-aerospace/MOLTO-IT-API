b73902e1b99cf118442347a1983bf9107e5353ba
dmoranteg <dmorante@ing.uc3m.es>
2019-07-03 02:04:31 +0200

    Change app

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 from flask import Flask, request, send_file
 import json
 import matlab.engine
-from time import time 
+from time import time
 
 app = Flask(__name__)
 eng = matlab.engine.start_matlab()
@@ -29,7 +29,6 @@ def init():
 
 @app.route('/sum/', methods=['GET','POST'])
 def sum():
-    
     if request.method == 'POST':
         tiempo_inicial = time() 
         data = request.get_json()
@@ -39,17 +38,15 @@ def sum():
         tiempo_final = time() 
         string = str(sum)
         tiempo_ejecucion = tiempo_final - tiempo_inicial
-        print("El tiempo de ejecuciÃ³n fue:", tiempo_ejecucion)
-        
+        print("El tiempo de ejecucion fue:", tiempo_ejecucion)
+
         return string
     elif request.method == 'GET':
         eng = matlab.engine.start_matlab()
         return "Motor Inicializado!!!"
-        
 @app.route('/sum/matlab', methods=['GET','POST'])
 def sum_matlab():
     global eng
-   
     if request.method == 'POST':
         tiempo_inicial = time() 
         data = request.get_json()
@@ -85,58 +82,50 @@ def optimization_mission():
         eng.quit()
         tiempo_ejecucion = tiempo_final - tiempo_inicial
         print("El tiempo de ejecucion fue:", tiempo_ejecucion)
-        return get_file(Ceres.txt)
+        return get_file('Ceres.txt')
 
     elif request.method == 'GET':
         eng = matlab.engine.start_matlab()
         return "Motor Inicializado."
- 
 
-
-        
 @app.route('/optimization/mission/json', methods=['GET', 'POST'])
 def optimization_mission_json():
     global eng
     if request.method == 'POST':
-        tiempo_inicial = time() 
-        
+        tiempo_inicial = time()
         data = request.get_json()
         print(data)
-        
-        with open('example.json', 'w') as json_file:  
+        with open('example.json', 'w') as json_file:
             json.dump(data, json_file)
+
         name = open('example.json', 'r').read()
         eng.molto_it_json(name)
-
-        
-        tiempo_final = time() 
-        
+        tiempo_final = time()
         eng.quit()
-        
         tiempo_ejecucion = tiempo_final - tiempo_inicial
-        
         print("El tiempo de ejecucion fue:", tiempo_ejecucion)
-        
         return get_file('~/tmp/Ceres/Results_extended.txt')
 
     elif request.method == 'GET':
         eng = matlab.engine.start_matlab()
         return "Motor Inicializado."
- 
+
 
 @app.route('/test', methods=['GET'])
 def test():
     global eng
-    return eng.figure(nargout=0)        
-
+    return eng.figure(nargout=0)
+Â
 @app.route('/get_file/<file_name>', methods=['GET', 'POST'])
 def get_file(file_name):
     if request.method == 'POST':
-        data = request.get_json()
-        return send_file(data[path], attachment_filename=file_name)
+    ÃÂ»data = request.get_json()
+     Â±return send_file(data[path], attachment_filename=file_name)
+
     elif request.method == 'GET':
         return send_file(file_name, attachment_filename=file_name)
-        
+
 if __name__ == '__main__':
 
      app.run(host="0.0.0.0")
+
